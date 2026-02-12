d1d21d3dd4de5623eafa718e2b832fbf7ac82800
vipul reddy gundalu <vipul@vipuls-MacBook-Air.local>
2026-02-12 12:15:10 +0530

    commit ke 1

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ variablity = False
 
 # change to your repo
 # repo_link = "git@github.com:aliifam/github-activity-generator.git" # ssh
-repo_link = "https://github.com/Vipulreddy15/log-analyzer-cli.git" # http
+repo_link = "https://github.com/Vipulreddy15/Net_device_info.git" # http
 
 tl = total_day #time day
 ctr = 1
